d9b4a694d92614825dd1e74041d8f54742fd3988
pab <pablo@corre.com>
2017-11-21 14:11:00 -1000

    Agregada consola

---
+++ src/tweeter.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/abiosoft/ishell"
+	"github.com/tweeter/src/service"
+)
+
+func main() {
+	shell := ishell.New()
+	shell.SetPrompt("Tweeter >> ")
+	shell.Print("Type 'help' to know commands\n")
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "publishTweet",
+		Help: "Publishes a Tweet",
+		Func: func(c *ishell.Context) {
+			defer c.ShowPrompt(true)
+			c.Print("Write you tweet: ")
+			tweet := c.ReadLine()
+			service.PublishTweet(tweet)
+			c.Print("Tweet sent\n")
+			return
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "showTweet",
+		Help: "Shows a tweet",
+		Func: func(c *ishell.Context) {
+			defer c.ShowPrompt(true)
+			tweet := service.GetTweet()
+			c.Println(tweet)
+			return
+		},
+	})
+
+	shell.Run()
+
+}
