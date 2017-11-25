7eb69e6450db5a2afeb902d5393c679d8d8617fd
pab <pablo@corre.com>
2017-11-24 15:22:47 -1000

    Interfaz tweet en proceo

--- src/domain/tweet.go
+++ src/domain/tweet.go
@@ -5,7 +5,17 @@ import (
 	"time"
 )
 
-type Tweet struct {
+type Tweet interface {
+	GetUser() string
+	GetText() string
+	GetId() int
+	IsRetweet() bool
+	RetweetedByWhom() string
+	SetId(id int) int
+	PrintableTweet() string
+}
+
+type TextTweet struct {
 	User        string
 	Text        string
 	Date        *time.Time
@@ -14,9 +24,53 @@ type Tweet struct {
 	RetweetedBy string
 }
 
-func NewTweet(user string, text string) *Tweet {
+type ImageTweet struct {
+	TextTweet
+	Image string
+}
+
+type QuoteTweet struct {
+	TextTweet
+	Quote Tweet
+}
+
+func NewTextTweet(user, text string) *TextTweet {
 	actualTime := time.Now()
-	tweet := Tweet{User: user, Text: text, Date: &actualTime, IsRetweet: false}
+	tweet := TextTweet{
+		User:      user,
+		Text:      text,
+		Date:      &actualTime,
+		IsRetweet: false,
+	}
+	return &tweet
+}
+
+func NewImageTweet(user, text, URL string) *ImageTweet {
+	actualTime := time.Now()
+	tweet := ImageTweet{
+		TextTweet: TextTweet{
+			User:      user,
+			Text:      text,
+			Date:      &actualTime,
+			IsRetweet: false,
+		},
+		Image: URL,
+	}
+	tweet.Image = URL
+	return &tweet
+}
+
+func NewQuoteTweet(user, text string, quote Tweet) *QuoteTweet {
+	actualTime := time.Now()
+	tweet := QuoteTweet{
+		TextTweet: TextTweet{
+			User:      user,
+			Text:      text,
+			Date:      &actualTime,
+			IsRetweet: false,
+		},
+		Quote: quote,
+	}
 	return &tweet
 }
 
@@ -26,10 +80,65 @@ func NewRetweet(originalTweet *Tweet, retweeter string) *Tweet {
 	return &tweet
 }
 
-func (tweet *Tweet) PrintableTweet() string {
+func (tweet *TextTweet) PrintableTweet() string {
 	return fmt.Sprintf("@%s: %s", tweet.User, tweet.Text)
 }
 
-func (tweet *Tweet) String() string {
-	return tweet.PrintableTweet()
+func (tweet *ImageTweet) PrintableTweet() string {
+	return fmt.Sprintf("@%s: %s %s", tweet.User, tweet.Text, tweet.Image)
+}
+
+func (tweet *QuoteTweet) PrintableTweet() string {
+	return fmt.Sprintf("@%s: %s \"@%s: %s\"", tweet.User, tweet.Text,
+		tweet.Quote.GetUser(), tweet.Quote.GetText())
+}
+
+// Setter and getters needed
+
+func (tweet *TextTweet) GetUser() string {
+	return tweet.User
+}
+
+func (tweet *ImageTweet) GetUser() string {
+	return tweet.User
+}
+
+func (tweet *QuoteTweet) GetUser() string {
+	return tweet.User
+}
+
+func (tweet *TextTweet) GetText() string {
+	return tweet.Text
+}
+
+func (tweet *ImageTweet) GetText() string {
+	return tweet.Text
+}
+
+func (tweet *QuoteTweet) GetText() string {
+	return tweet.Text
+}
+
+func (tweet *TextTweet) GetId() int {
+	return tweet.Id
+}
+
+func (tweet *ImageTweet) GetId() int {
+	return tweet.Id
+}
+
+func (tweet *QuoteTweet) GetId() int {
+	return tweet.Id
+}
+
+func (tweet *TextTweet) SetId(id int) {
+	tweet.Id = id
+
+}
+func (tweet *ImageTweet) SetId(id int) {
+	tweet.Id = id
+}
+
+func (tweet *QuoteTweet) SetId(id int) {
+	tweet.Id = id
 }
